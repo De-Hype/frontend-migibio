893569260ac0f7e82827912d9d56a5b3da06d249
David Hype <121057038+De-Hype@users.noreply.github.com>
2023-11-25 06:11:26 +0100

    Update server.js

--- utils/server.js
+++ utils/server.js
@@ -1,2 +1,2 @@
-export const Backend_Url = `https://easytan-crayfish-wig.cyclic.app/api/user`
+export const Backend_Url = `https://easy-tan-crayfish-wig.cyclic.app/api/user`
 // export const Backend_Url = 'http://localhost:5000/api/user'
